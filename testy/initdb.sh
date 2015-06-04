48b075071407c61907991adb4ada5bc4294dd4d8
William A Adams <spam@nanotechstyles.com>
2015-06-04 08:40:37 -0700

    Added database initialization script

---
+++ testy/initdb.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+ovsdb-server --remote=punix:/usr/local/var/run/openvswitch/db.sock \
+                     --remote=db:Open_vSwitch,Open_vSwitch,manager_options \
+                     --private-key=db:Open_vSwitch,SSL,private_key \
+                     --certificate=db:Open_vSwitch,SSL,certificate \
+                     --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert \
+                     --pidfile --detach
+
+# initialize database
+ovs-vsctl --no-wait init
+
+
+# start openvswitch daemon
+ovs-vswitchd --pidfile --detach
